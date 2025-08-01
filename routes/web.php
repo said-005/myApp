9e53a1521953d638030b552e2e734419e886fadc
said-005 <saidnaoume@gmail.com>
2025-08-01 16:44:35 +0200

    the first version mofified

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
-
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
 
+Route::get('/', function () {
+    return File::get(public_path('index.html'));
+});
 require __DIR__.'/auth.php';
+Route::get('/{any}', function () {
+    return File::get(public_path('index.html'));
+})->where('any', '^(?!login$|logout$).*$');
